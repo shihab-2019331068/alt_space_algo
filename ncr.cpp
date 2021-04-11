701cb1e21efe5f7e01ab03609cd649bfab06f204
shihab-2019331068 <69476389+shihab-2019331068@users.noreply.github.com>
2021-04-11 09:13:52 +0600

    Update ncr.cpp

--- ncr.cpp
+++ ncr.cpp
@@ -1,19 +1,19 @@
-ll bmod(ll n,ll k)
+ll bmd(ll n,ll k)
 {
    if(!k)
       return 1;
    if(k%2)
-      return (n*bmod(n,k-1))%M;
-   ll R=bmod(n,k/2);
+      return (n*bmd(n,k-1))%M;
+   ll R=bmd(n,k/2);
    return (R*R)%M;
 }
 
 ll m_inv(ll x)
 {
-   return bmod(x,M-2);
+   return bmd(x,M-2);
 }
 
 ll ncr(ll n,ll r)
 {
-   return F[n]*m_inv(F[r]*F[n-r]%M)%M;|
+   return F[n]*m_inv(F[r]*F[n-r]%M)%M;
 }
