513edbba86ca00fa722060d68d226e0cf9ef26e2
shihab-2019331068 <69476389+shihab-2019331068@users.noreply.github.com>
2021-04-13 06:27:50 +0600

    Create ttn.cpp

---
+++ ttn.cpp
@@ -0,0 +1,12 @@
+vector<ll> ttn(vector<ll> &a)
+{
+   bitset<N> t=1;
+   vector<ll> A;
+   ll i;
+   for(ll i:a)
+      t|=(t<<i);
+   for(i=0; i<N; i++)
+      if(t[i])
+         A.push_back(i);|
+   return A;
+}
