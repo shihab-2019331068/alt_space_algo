93250118e0eed7176b81c774bd7449a43a292243
shihab-2019331068 <69476389+shihab-2019331068@users.noreply.github.com>
2021-04-26 20:01:18 +0600

    Update kmp.cpp

--- kmp.cpp
+++ kmp.cpp
@@ -1,9 +1,32 @@
-for(j=0,i=1; i<tz; i++){
-   if(t[j]==t[i])
-      p[i]=++j;
-   else if(j){
-      j=p[j-1]; i--;
+void mlps(string &t,vector<ll> &p)
+{
+   ll i,j,tz=t.size();
+   for(j=0,i=1; i<tz; i++){
+      if(t[j]==t[i])
+         p[i]=++j;
+      else if(j){
+         j=p[j-1]; i--;
+      }
+      else
+         p[i]=0;
    }
-   else
-      p[i]=0;
-}|
+}
+
+ll kmp(string &s,string &t)
+{
+   ll tz=t.size(), sz=s.size();
+   vector<ll> p(tz);
+   mlps(t,p);
+   ll i,j;
+   for(i=j=0; i<sz; ){
+      if(s[i]==t[j]){
+         i++; j++;
+      }
+      if(j==tz)
+         return i-j;
+      if(i<sz and s[i]!=t[j]){
+         j?j=p[j-1]:i++;
+      }
+   }
+   return sz;|
+}
