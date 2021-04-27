877e1444837ba98f3915fa10e2a0dd61ecdb0a72
shihab-2019331068 <69476389+shihab-2019331068@users.noreply.github.com>
2021-04-27 09:03:13 +0600

    Update stree.cpp

--- stree.cpp
+++ stree.cpp
@@ -1,13 +1,28 @@
-ll inq(ll l,ll r,ll p,ll A,ll sp,ll ep)
+ll rnq(ll sp,ll ep,ll l=1,ll r=n,ll p=0)
 {
    if(ep<l or r<sp)
-      return A;
+      return 0;
    if(sp<=l and r<=ep)
-      return min(A,t[p]);
+      return t[p];
    ll m,lc,rc;
    m=(l+r)/2;
    lc=p*2+1; rc=lc+1;
-   return min(inq(l,m,lc,A,sp,ep),inq(m+1,r,rc,A,sp,ep));
+   return rnq(sp,ep,l,m,lc)+rnq(sp,ep,m+1,r,rc);
+}
+
+void upd(ll i,ll v,ll l=1,ll r=n,ll p=0)
+{
+   if(i<l or r<i)
+      return;
+   if(l==r and r==i){
+      t[p]=a[l]=v;
+      return;
+   }
+   ll m,lc,rc;
+   m=(l+r)/2;
+   lc=p*2+1; rc=lc+1;
+   upd(i,v,l,m,lc); upd(i,v,m+1,r,rc);
+   t[p]=t[lc]+t[rc];
 }
 
 void build(ll l=1,ll r=n,ll p=0)
@@ -20,5 +35,6 @@ void build(ll l=1,ll r=n,ll p=0)
    m=(l+r)/2;
    lc=p*2+1; rc=lc+1;
    build(l,m,lc); build(m+1,r,rc);
-   t[p]=min(t[lc],t[rc]);
-}|
+   t[p]=t[lc]+t[rc];
+}
+|
