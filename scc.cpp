a1964f07fb8efed47b5c96297897d038ec0c3ee8
shihab-2019331068 <69476389+shihab-2019331068@users.noreply.github.com>
2021-04-29 05:42:19 +0600

    Create scc.cpp

---
+++ scc.cpp
@@ -0,0 +1,19 @@
+void dfs(ll u=1,ll p=-1,ll D=0)
+{
+   d[u]=D;
+   for(auto [i,v]:g[u]){
+      if(v==p)
+         continue;
+      if(d[v]==-1)
+         dfs(v,u,D+1);
+      if(d[v]>D){
+         A.push_back({u,v});
+         A.push_back({v,u});
+      }
+      else if(f[i]){
+         f[i]=0;
+         A.push_back({u,v});
+      }
+      d[u]=min(d[u],d[v]);|
+   }
+}
