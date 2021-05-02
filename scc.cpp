04329fe4c7e2422d1003b7a3b6ce9da9c1463bf7
shihab-2019331068 <69476389+shihab-2019331068@users.noreply.github.com>
2021-05-02 17:42:17 +0600

    Update scc.cpp

--- scc.cpp
+++ scc.cpp
@@ -1,4 +1,4 @@
-void scc(ll u=1,ll p=-1,ll D=0)
+void scc(ll u,ll p,ll D)
 {
    d[u]=D;
    for(auto [i,v]:g[u]){
@@ -6,14 +6,6 @@ void scc(ll u=1,ll p=-1,ll D=0)
          continue;
       if(d[v]==-1)
          scc(v,u,D+1);
-      if(d[v]>D){
-         A.push_back({u,v});
-         A.push_back({v,u});
-      }
-      else if(f[i]){
-         f[i]=0;
-         A.push_back({u,v});
-      }
       d[u]=min(d[u],d[v]);|
    }
 }
