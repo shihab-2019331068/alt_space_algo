e8e108fad3a6a591574b53e2b534570604bcf085
shihab-2019331068 <69476389+shihab-2019331068@users.noreply.github.com>
2021-05-09 21:12:27 +0600

    Update lcs.cpp

--- lcs.cpp
+++ lcs.cpp
@@ -5,7 +5,7 @@ ll lcs(string &A,string &B)
    for(i=0; i<n; i++){
       for(j=0; j<m; j++){
          f[i][j]=i?max(f[i][j],f[i-1][j]):0;
-         f[i][j]=j?max(f[i][j],f[i][j-1]):0;
+         f[i][j]=j?max(f[i][j],f[i][j-1]):f[i][j];
          if(A[i]==B[j])
             f[i][j]=(i*j)?max(f[i][j],f[i-1][j-1]+1):1;
       }
