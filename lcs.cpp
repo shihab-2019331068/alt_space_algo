06c98048c3a3ada7f0e4d1823c53f9cdea33ff38
shihab-2019331068 <69476389+shihab-2019331068@users.noreply.github.com>
2021-05-09 20:40:18 +0600

    Create lcs.cpp

---
+++ lcs.cpp
@@ -0,0 +1,14 @@
+ll lcs(string &A,string &B)
+{
+   ll i,j,n,m;
+   n=A.size(); m=B.size();
+   for(i=0; i<n; i++){
+      for(j=0; j<m; j++){
+         f[i][j]=i?max(f[i][j],f[i-1][j]):0;
+         f[i][j]=j?max(f[i][j],f[i][j-1]):0;
+         if(A[i]==B[j])
+            f[i][j]=(i*j)?max(f[i][j],f[i-1][j-1]+1):1;
+      }
+   }
+   return f[n-1][m-1];|
+}
