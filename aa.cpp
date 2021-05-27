7a575f815d3e8b5227fd79424d984079dae66475
shihab-2019331068 <69476389+shihab-2019331068@users.noreply.github.com>
2021-05-27 17:00:20 +0600

    Create aa.cpp

---
+++ aa.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define fastio            ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define ifs               freopen("aa.in","r",stdin);
+#define ofs               freopen("aa.out","w",stdout);
+#define iof               ifs ofs
+#define ll                long long
+#define ld                long double
+#define da(x,i)           cout << #x << "[" << i << "]=" << x[i] << "\n";
+#define da2(x,i,y,j)      cout << #x << "[" << i << "]=" << x[i] << " ! " << #y << "[" << j << "]=" << y[j] << "\n";
+#define d2a(x,i,j)        cout << #x << "[" << i << "][" << j << "]=" << x[i][j] << "\n";
+#define db(x)             cout << #x << "=" << (x) << "\n";
+#define db2(x,y)          cout << #x << "=" << (x) << " ! " << #y << "=" << (y) << "\n";
+#define db3(x,y,z)        cout << #x << "=" << (x) << " ! " << #y << "=" << (y) << " ! " << #z << "=" << (z) << "\n";
+#define db4(w,x,y,z)      cout << #w << "=" << (w) << " ! " << #x << "=" << (x) << " ! " << #y << "=" << (y) << " ! " << #z << "=" << (z) << "\n";
+#define inf               LLONG_MAX
+#define fr                first
+#define sc                second
+#define pll               pair<ll,ll> 
+#define vll               vector<ll> 
+#define all(a)            a.begin(),a.end()
+
+
+
+
+
+int main()
+{
+   
+}
+
+
+
+
+
+
+
+/* stuff you should look for
+   * obvious case
+   * int overflow, array bounds
+   * special cases (n=1?)
+   * do smth instead of nothing and stay organized
+   * WRITE STUFF DOWN
+   * DON'T GET STUCK ON ONE APPROACH
+*/
