b5e30363eccb80b04525888f3ab1660092b982ce
shihab-2019331068 <69476389+shihab-2019331068@users.noreply.github.com>
2021-06-05 05:21:23 +0600

    Update aa.cpp

--- aa.cpp
+++ aa.cpp
@@ -8,32 +8,28 @@ using namespace std;
 #define iof               ifs ofs
 #define ll                long long
 #define ld                long double
-#define da(x,i)           cout << #x << "[" << i << "]=" << x[i] << "\n";
-#define da2(x,i,y,j)      cout << #x << "[" << i << "]=" << x[i] << " ! " << #y << "[" << j << "]=" << y[j] << "\n";
-#define d2a(x,i,j)        cout << #x << "[" << i << "][" << j << "]=" << x[i][j] << "\n";
-#define db(x)             cout << #x << "=" << (x) << "\n";
-#define db2(x,y)          cout << #x << "=" << (x) << " ! " << #y << "=" << (y) << "\n";
-#define db3(x,y,z)        cout << #x << "=" << (x) << " ! " << #y << "=" << (y) << " ! " << #z << "=" << (z) << "\n";
-#define db4(w,x,y,z)      cout << #w << "=" << (w) << " ! " << #x << "=" << (x) << " ! " << #y << "=" << (y) << " ! " << #z << "=" << (z) << "\n";
 #define inf               LLONG_MAX
 #define fr                first
 #define sc                second
-#define pll               pair<ll,ll> 
-#define vll               vector<ll> 
+#define pll               pair<ll,ll>
+#define vll               vector<ll>
 #define all(a)            a.begin(),a.end()
 
 
+#define db(args...) { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); err(_it, args); }
 
+void err(istream_iterator<string> it) {}
+template<typename T, typename... Args>
+void err(istream_iterator<string> it, T a, Args... args) {
+	cerr << *it << " = " << a << " ! ";
+	err(++it, args...);
+}
 
 
 int main()
 {
-   
+   |
 }
-
-
-
-
 
 
 
@@ -45,3 +41,28 @@ int main()
    * WRITE STUFF DOWN
    * DON'T GET STUCK ON ONE APPROACH
 */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
