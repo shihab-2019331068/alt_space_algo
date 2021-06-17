bcfa91449129fd9ab2fe7d89f2f4bc0d0576b16f
shihab-2019331068 <69476389+shihab-2019331068@users.noreply.github.com>
2021-06-17 10:04:59 +0600

    Update sieve.cpp

--- sieve.cpp
+++ sieve.cpp
@@ -2,10 +2,10 @@ void sieve()
 {
    ll i,j;
    for(i=2; i<N; i++){
-      if(f[i])
+      if(f1[i])
          continue;
       p.push_back(i);|
       for(j=i; j<N; j+=i)
-         f[j]++;
+         f1[j]++;
    }
 }
