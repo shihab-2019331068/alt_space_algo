a0e4d00487e0ae3a1e021c328eb673e30feaef48
shihab-2019331068 <69476389+shihab-2019331068@users.noreply.github.com>
2021-07-20 08:55:45 +0600

    Update b_s.cpp

--- b_s.cpp
+++ b_s.cpp
@@ -1,10 +1,8 @@
 ll b_s(ll l,ll r)
 {
-   if(l==r)
-      return l;
+   if(l==r) return l;
    ll m;
    m=(l+r)/2;
-   if(chk(m))
-      return b_s(l,m);
+   if(chk(m)) return b_s(l,m);
    return b_s(m+1,r);
 }
