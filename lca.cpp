a199bfe0b853e9edae6206eb75146fe02dd9d955
shihab-2019331068 <69476389+shihab-2019331068@users.noreply.github.com>
2021-08-01 17:24:48 +0600

    Update lca.cpp

--- lca.cpp
+++ lca.cpp
@@ -1,48 +1,33 @@
-ll F[N],a[N],t[N],I;
-
-ll rnq(ll sp,ll ep,ll l=1,ll r=I,ll p=0)
+void dfs(ll u,ll i)
 {
-   if(ep<l or r<sp)
-      return inf;
-   if(sp<=l and r<=ep)
-      return t[p];
-   ll m,lc,rc;
-   m=(l+r)/2;
-   lc=p*2+1; rc=lc+1;
-   return min(rnq(sp,ep,l,m,lc),rnq(sp,ep,m+1,r,rc));
-}
-
-void build(ll l=1,ll r=I,ll p=0)
-{
-   if(l==r){
-      t[p]=a[l];
-      return;
+   if(i){
+      P[u][i]=P[P[u][i-1]][i-1];
    }
-   ll m,lc,rc;
-   m=(l+r)/2;
-   lc=p*2+1; rc=lc+1;
-   build(l,m,lc); build(m+1,r,rc);
-   t[p]=min(t[lc],t[rc]);
-}
-
-ll ret_dis(ll u,ll v)
-{
-   if(u==v) return 0;
-   ll l,r,h1,h2,h,d;
-   l=F[u]; r=F[v];
-   if(l>r) swap(l,r);
-   h=rnq(l,r); h1=a[l]; h2=a[r];
-   d=h1+h2-h*2;
-   return d;
-}
-
-void dfs(ll u=1,ll p=0,ll h=1)
-{
-   a[++I]=h;
-   if(!F[u]) F[u]=I;
    for(ll v:g[u]){
-      if(v==p) continue;
-      dfs(v,u,h+1);
-      a[++I]=h;
+      if(P[u][0]==v) continue;
+      P[v][0]=u; h[v]=h[u]+1;
+      dfs(v,i);
    }
-}|
+}
+
+void build()
+{
+   ll i;
+   for(i=0; i<N1; i++) dfs(1,i);
+}
+
+ll lca(ll l,ll r)
+{
+   ll R,d,i;
+   if(h[l]<h[r]) swap(l,r);
+   d=h[l]-h[r];
+   for(i=0; i<N1; i++)
+      if(d>>i&1) l=P[l][i];
+   if(l==r) return l;
+   for(i=N1-1; i+1; i--){
+      if(P[l][i]!=P[r][i]){
+         l=P[l][i]; r=P[r][i];
+      }
+   }
+   return P[l][0];|
+}
