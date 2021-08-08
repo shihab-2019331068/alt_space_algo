a0229ebfa13c85dd17bd2c3c16cbeaf7f258a125
shihab-2019331068 <69476389+shihab-2019331068@users.noreply.github.com>
2021-08-08 17:52:12 +0600

    Update aa.cpp

--- aa.cpp
+++ aa.cpp
@@ -3,9 +3,6 @@
 using namespace std;
 
 #define fastio            ios_base::sync_with_stdio(false); cin.tie(NULL);
-#define ifs               freopen("aa.in","r",stdin);
-#define ofs               freopen("aa.out","w",stdout);
-#define iof               ifs ofs
 #define ll                long long
 #define ld                long double
 #define fr                first
@@ -15,6 +12,7 @@ using namespace std;
 #define vll               vector<ll>
 #define bp(x)             __builtin_popcountll(x)
 #define all(a)            a.begin(),a.end()
+#define cb(a,i)           (a>>i&1)
 
 
 #define db(args...) { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); err(_it, args); }
