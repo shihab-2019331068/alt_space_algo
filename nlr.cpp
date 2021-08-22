8d5d5b007f2ce841b348674468ff6c8c3aed3fab
shihab-2019331068 <69476389+shihab-2019331068@users.noreply.github.com>
2021-08-22 19:09:46 +0600

    Create nlr.cpp

---
+++ nlr.cpp
@@ -0,0 +1,15 @@
+void nlr()
+{
+   vector<pll> v={mp(0,n+1);
+   for(i=n; i; i--){
+      for(; v.back()->fr>=h[i]; v.pop_back());
+      R[i]=v.back()->sc;
+      v.push_back(mp(h[i],i));
+   }
+   v={mp(0,0)};
+   for(i=1; i<=n; i++){
+      for(; v.back()->fr>=h[i]; v.pop_back());
+      L[i]=v.back()->sc;
+      v.push_back(mp(h[i],i));|
+   }
+}
