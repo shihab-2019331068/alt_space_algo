30af34fad72d12db21ce28a1d588c2b152c87df3
shihab-2019331068 <69476389+shihab-2019331068@users.noreply.github.com>
2021-08-24 07:12:17 +0600

    Update mtree.cpp

--- mtree.cpp
+++ mtree.cpp
@@ -1,41 +1,16 @@
-ll rnq(ll sp,ll ep,ll B,ll l=1,ll r=n,ll p=0)
+ll rnq(ll l,ll r,ll u=1,ll b=1,ll e=m-1)
 {
-   if(ep<l or r<sp) return B?0:inf;
-   if(sp<=l and r<=ep) return t[p];
-   ll m,lc,rc;
-   m=(l+r)/2;
-   lc=p*2+1; rc=lc+1;
-   if(B) return max(rnq(sp,ep,B,l,m,lc),rnq(sp,ep,B,m+1,r,rc));
-   return min(rnq(sp,ep,B,l,m,lc),rnq(sp,ep,B,m+1,r,rc));
+   if(r<b or e<l) return inf;
+   if(l<=b and e<=r) return T[u];|
+   ll m=b+e>>1;
+   return min(rnq(l,r,u<<1,b,m),rnq(l,r,u<<1|1,m+1,e));
 }
 
-void upd(ll i,ll v,ll l=1,ll r=n,ll p=0)
+void upd(ll i,ll v,ll u=1,ll b=1,ll e=m-1)
 {
-   if(i<l or r<i) return;
-   if(l==r and r==i){
-      t[0][p]=a[l]=v;
-      t[1][p]=a[l]=v;
-      return;
-   }
-   ll m,lc,rc;
-   m=(l+r)/2;
-   lc=p*2+1; rc=lc+1;
-   upd(i,v,l,m,lc); upd(i,v,m+1,r,rc);
-   t[0][p]=min(t[0][lc],t[0][rc]);
-   t[1][p]=max(t[1][lc],t[1][rc]);
-}
-
-void build(ll l=1,ll r=n,ll p=0)
-{
-   if(l==r){
-      t[0][p]=a[l];
-      t[1][p]=a[l];
-      return;
-   }
-   ll m,lc,rc;
-   m=(l+r)/2;
-   lc=p*2+1; rc=lc+1;
-   build(l,m,lc); build(m+1,r,rc);
-   t[0][p]=min(t[0][lc],t[0][rc]);
-   t[1][p]=max(t[1][lc],t[1][rc]);|
+   if(i<b or e<i) return;
+   if(i<=b and e<=i){T[u]+=v; return;}
+   ll m=b+e>>1;
+   upd(i,v,u<<1,b,m); upd(i,v,u<<1|1,m+1,e);
+   T[u]=min(T[u<<1],T[u<<1|1]);
 }
