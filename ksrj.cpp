13d2b0d10eacb6c91703d7a55750a2233bd9439b
shihab-2019331068 <69476389+shihab-2019331068@users.noreply.github.com>
2021-08-26 07:04:28 +0600

    Update ksrj.cpp

--- ksrj.cpp
+++ ksrj.cpp
@@ -1,33 +1,24 @@
-void dfs1(ll u)
+void dfs(ll u,ll P,aa &g,aa &vs,aa &p,aa &sk)
 {
-   vs[u]++;
+   vs[u]++; p[u]=P;
    for(ll v:g[u])
       if(!vs[v])
-         dfs1(v);
-   sk.push_back(u);
+         dfs(v,P,g,vs,p,sk);
+   if(P==-1) sk.push_back(u);
 }
 
-void dfs2(ll u,ll D)
-{
-   vs[u]++;
-   d[u]=D;
-   for(ll v:rg[u])
-      if(!vs[v])
-         dfs2(v,D);
-}
-
-void ksrj()
+vll ksrj(ll n,aa &g,aa &rg)
 {
    ll i,u;
-   sk={};
-   vs.assign(n+1,0);
+   vector<ll> sk,vs(n+1,0),p(n+1,0);
    for(i=1; i<=n; i++)
       if(!vs[i])
-         dfs1(i);
-   vs.assign(n+1,0); d=vs;
+         dfs(i,-1,g,vs,p,sk);
+   p=vs=vll(n+1,0);
    for(; sk.size(); sk.pop_back()){
       u=sk.back();
       if(!vs[u])
-         dfs2(u,u);|
+         dfs(u,u,rg,vs,p,sk);
    }
+   return p;|
 }
