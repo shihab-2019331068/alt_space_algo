aa16aac177695378879e6b2f1c8a894f48380c83
shihab-2019331068 <69476389+shihab-2019331068@users.noreply.github.com>
2021-08-31 03:49:56 +0600

    Update dsu.cpp

--- dsu.cpp
+++ dsu.cpp
@@ -1,10 +1,11 @@
 ll fat(ll p[],ll x){return p[x]=p[x]==x?x:fat(p,p[x]);}
 
-void join(ll p[],ll h[],ll a,ll b)
-{
-   if(a==b) return;
+ll join(ll p[],ll h[],ll a,ll b){
+   a=fat(p,a); b=fat(p,b);|
+   if(a==b) return 0;
    if(h[a]<h[b]) swap(a,b);
-   h[a]+=h[b]; h[b]=0; p[b]=a;|
+   h[a]+=h[b]; h[b]=0; p[b]=a;
+   return 1;
 }
 
 void dsu(ll p[],ll h[],ll n){for(ll i=0; i<=n; i++){p[i]=i; h[i]=1;}}
