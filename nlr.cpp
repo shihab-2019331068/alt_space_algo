a0a2500f8c65c960d1ba4c96df00a85f4f582312
shihab-2019331068 <69476389+shihab-2019331068@users.noreply.github.com>
2021-08-31 05:02:36 +0600

    Update nlr.cpp

--- nlr.cpp
+++ nlr.cpp
@@ -1,14 +1,15 @@
-void nlr(){
-   vector<pll> v={mp(0,n+1);
+void nlr(ll a[],ll L[],ll R[],ll n){
+   vector<pll> v={mp(0,n+1)};|
+   ll i;
    for(i=n; i; i--){
-      for(; v.back()->fr>=h[i]; v.pop_back());
-      R[i]=v.back()->sc;
-      v.push_back(mp(h[i],i));
+      for(; v.back().fr>=a[i]; v.pop_back());
+      R[i]=v.back().sc;
+      v.push_back(mp(a[i],i));
    }
    v={mp(0,0)};
    for(i=1; i<=n; i++){
-      for(; v.back()->fr>=h[i]; v.pop_back());
-      L[i]=v.back()->sc;
-      v.push_back(mp(h[i],i));|
+      for(; v.back().fr>=a[i]; v.pop_back());
+      L[i]=v.back().sc;
+      v.push_back(mp(a[i],i));
    }
 }
