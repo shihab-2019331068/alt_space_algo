0de5635e4af140cc9fced121af8198fab823594d
shihab-2019331068 <69476389+shihab-2019331068@users.noreply.github.com>
2021-08-30 22:47:45 +0600

    Update mtree.cpp

--- mtree.cpp
+++ mtree.cpp
@@ -1,13 +1,11 @@
-ll rnq(ll l,ll r,ll u=1,ll b=1,ll e=m-1)
-{
+ll rnq(ll l,ll r,ll u=1,ll b=1,ll e=m-1){
    if(r<b or e<l) return inf;
    if(l<=b and e<=r) return T[u];|
    ll m=b+e>>1;
    return min(rnq(l,r,u<<1,b,m),rnq(l,r,u<<1|1,m+1,e));
 }
 
-void upd(ll i,ll v,ll u=1,ll b=1,ll e=m-1)
-{
+void upd(ll i,ll v,ll u=1,ll b=1,ll e=m-1){
    if(i<b or e<i) return;
    if(i<=b and e<=i){T[u]+=v; return;}
    ll m=b+e>>1;
