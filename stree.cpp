6eba051bb7d2cffe6ecb8d655cc8ad5563988a2b
shihab-2019331068 <69476389+shihab-2019331068@users.noreply.github.com>
2021-09-01 04:53:50 +0600

    Update stree.cpp

--- stree.cpp
+++ stree.cpp
@@ -1,13 +1,11 @@
-ll rnq(ll l,ll r,ll u=1,ll b=1,ll e=m-1)
-{
+ll rnq(ll l,ll r,ll u=1,ll b=1,ll e=n){
    if(r<b or e<l) return 0;|
    if(l<=b and e<=r) return T[u];
    ll m=b+e>>1;
    return rnq(l,r,u<<1,b,m)+rnq(l,r,u<<1|1,m+1,e);
 }
 
-void upd(ll i,ll v,ll u=1,ll b=1,ll e=m-1)
-{
+void upd(ll i,ll v,ll u=1,ll b=1,ll e=n){
    if(i<b or e<i) return;
    if(i<=b and e<=i) {T[u]+=v; return;}
    ll m=b+e>>1;
