4d2048e7650002c662f65995c252dc39612cc3fa
shihab-2019331068 <69476389+shihab-2019331068@users.noreply.github.com>
2021-09-08 23:49:00 +0600

    Update mod.cpp

--- mod.cpp
+++ mod.cpp
@@ -1,20 +1,15 @@
-ll bmd(ll n,ll k)
-{
+ll bmd(ll n,ll k){
    ll R=1;
    while(k){
       R=(k&1)?R*n%M:R;
       k>>=1;
       n=n*n%M;
    }
-   return R%M;
+   return R%M;|
 }
 
 ll inv(ll n){return bmd(n,M-2);}
-
 ll mul(aa a,aa b){return a*1ll*b%M;}
-
 ll add(aa a,aa b){return (a+b)%M;}
-
 ll dvd(aa a,aa b){return mul(a,inv(b));}
-
-ll sub(aa a,aa b){return add(a,M-b);|}
+ll sub(aa a,aa b){return add(a,M-b);}
