91da1ee044556d0b1fed2219c11673f621c5c9a1
shihab-2019331068 <69476389+shihab-2019331068@users.noreply.github.com>
2021-09-13 14:29:37 +0600

    Update scc.cpp

--- scc.cpp
+++ scc.cpp
@@ -1,9 +1,22 @@
-void scc(ll u,ll p)
-{
-   d[u]=++D;
+ll fat(ll p[],ll x){return p[x]=p[x]==x?x:fat(p,p[x]);}
+
+ll join(ll p[],ll h[],ll a,ll b){
+   a=fat(p,a); b=fat(p,b);
+   if(a==b) return 0;
+   if(h[a]<h[b]) swap(a,b);
+   h[a]+=h[b]; h[b]=0; p[b]=a;
+   return 1;
+}
+
+void dsu(ll p[],ll h[],ll n){for(ll i=0; i<=n; i++){p[i]=i; h[i]=1;}}
+
+ll en_t[N],lo_t[N],time=0;
+void scc(ll u,ll p){
+   en_t[u]=lo_t[u]=++time;
    for(auto [i,v]:g[u]){
       if(v==p) continue;
-      if(d[v]==-1) scc(v,u);
-      d[u]=min(d[u],d[v]);
+      if(!en_t[v]) scc(v,u);
+      lo_t[u]=min(lo_t[u],lo_t[v]);
+      if(en_t[u]>=lo_t[v]) join(p,h,u,v);|
    }
 }
