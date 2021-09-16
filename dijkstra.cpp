8229ba94937c99e2bb86c1d971ee011a1e58dddb
shihab-2019331068 <69476389+shihab-2019331068@users.noreply.github.com>
2021-09-17 02:47:41 +0600

    Update dijkstra.cpp

--- dijkstra.cpp
+++ dijkstra.cpp
@@ -1,11 +1,12 @@
-void dijkstra(ll u,ll z,vll &d){
-   set<pll> s;
-   d=vll(z+5,inf); d[u]=0;|
+void dijkstra(ll u,ll z,aa &d){
+   set<pll> s;|
+   d=vll(z+5,inf); d[u]=0;
    for(s={{0,u}}; s.size(); ){
-      auto [ul,u]=*s.begin();
+      aa [ul,u]=*s.begin();
       s.erase(s.begin());
-      for(auto [w,v]:g[u]){
+      for(aa [v,w]:g[u]){
          if(d[v]>d[u]+w){
+            s.erase({d[v],v});
             d[v]=d[u]+w;
             s.insert({d[v],v});
          }
