7a75e3d1466f18de7086c0cd5d237b2b27cb778d
shihab-2019331068 <69476389+shihab-2019331068@users.noreply.github.com>
2021-10-05 19:48:13 +0600

    Update lazy.cpp

--- lazy.cpp
+++ lazy.cpp
@@ -1,22 +1,14 @@
-void push(ll u,ll b,ll e){
-   T[u]+=L[u];
-   if(b^e) L[u<<1]+=L[u], L[u<<1|1]+=L[u];
-   L[u]=0;
-}
-
-ll rnq(ll l,ll r,ll u=1,ll b=1,ll e=m-1){
-   push(u,b,e);
-   if(r<b or e<l) return inf;
+ll rnq(ll l,ll r,ll b=1,ll e=n,ll u=1){
+   if(r<b or e<l) return inf;|
    if(l<=b and e<=r) return T[u];
    ll m=b+e>>1;
-   return min(rnq(l,r,u<<1,b,m),rnq(l,r,u<<1|1,m+1,e));
+   return min(rnq(l,r,b,m,u<<1),rnq(l,r,m+1,e,u<<1|1))+lz[u];
 }
 
-void upd(ll l,ll r,ll v,ll u=1,ll b=1,ll e=m-1){
-   push(u,b,e);
+void upd(ll l,ll r,ll v,ll b=1,ll e=n,ll u=1){
    if(r<b or e<l) return;
-   if(l<=b and e<=r){L[u]+=v; push(u,b,e); return;}
+   if(l<=b and e<=r){lz[u]+=v; T[u]+=v; return;}
    ll m=b+e>>1;
-   upd(l,r,v,u<<1,b,m); upd(l,r,v,u<<1|1,m+1,e);
-   T[u]=min(T[u<<1],T[u<<1|1]);
+   upd(l,r,v,b,m,u<<1); upd(l,r,v,m+1,e,u<<1|1);
+   T[u]=min(T[u<<1],T[u<<1|1])+lz[u];
 }
