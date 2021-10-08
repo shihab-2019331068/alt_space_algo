8a0c4e5fc11315d9d369ee5800476b71123fca00
shihab-2019331068 <69476389+shihab-2019331068@users.noreply.github.com>
2021-10-08 15:12:06 +0600

    Update fws.cpp

--- fws.cpp
+++ fws.cpp
@@ -1,12 +1,11 @@
-void fws()
-{
-   ll i,j,k;
+void fws(){
+   ll i,j,k;|
    for(k=0; k<n; k++){
       for(i=0; i<n; i++){
          for(j=0; j<n; j++){
             if(max(d[i][k],d[k][j])==inf)
                continue;
-            d[i][j]=min(d[i][j],d[i][k]+d[k][j]);|
+            d[i][j]=min(d[i][j],d[i][k]+d[k][j]);
          }
       }
    }
