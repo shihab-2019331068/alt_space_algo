770b6617289b95ff13dab78e20fcfed672f0c154
shihab-2019331068 <69476389+shihab-2019331068@users.noreply.github.com>
2021-10-29 02:35:59 +0600

    Update aa.cpp

--- aa.cpp
+++ aa.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 #define fastio            ios_base::sync_with_stdio(false); cin.tie(NULL);
 #define aa                auto
+#define cn                constant
 #define ll                long long
 #define ld                long double
 #define fr                first
@@ -16,9 +17,10 @@ using namespace std;
 #define arr(a)            a.rbegin(),a.rend()
 #define cb(a,i)           (a>>i&1)
 #define mp(a,b)           make_pair(a,b)
+#define pb(a)             push_back(a)
 
 
-#define db(args...) { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); err(_it, args); }
+#define db(args...) { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); err(_it, args); cerr<<"\n";}
 void err(istream_iterator<string> it) {}
 template<typename T, typename... Args>
 void err(istream_iterator<string> it, T a, Args... args) {
@@ -26,14 +28,17 @@ void err(istream_iterator<string> it, T a, Args... args) {
    err(++it, args...);
 }
 
-const ll N=5e5+7,N1=25,inf=2e18;
-const ld eps=1e-8;
-const ll M=1e9+7;
-//const ll M=998244353;
+cn ll N=5e5+7,N1=25,inf=2e18;
+cn ld eps=1e-8;
+cn ll M=1e9+7;
+//cn ll M=998244353;
 
 int main(){
    |
 }
+
+
+
 
 
 
