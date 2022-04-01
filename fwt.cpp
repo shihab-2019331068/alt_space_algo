1167400225a14067eb15248b3f51ae18a7082039
shihab-2019331068 <69476389+shihab-2019331068@users.noreply.github.com>
2022-04-01 11:46:41 +0600

    Update fwt.cpp

--- fwt.cpp
+++ fwt.cpp
@@ -1,2 +1,2 @@
-void upd(ll i,ll x){for(;i<N;i+=i&(-i)) t[i]+=x;}
-ll ask(ll i){ll R=0; for(; i; i-=i&(-i)) R+=t[i]; return R;|}
+void upd(aa &t,ll i,ll v) {for(; i<t.size(); i+=i&-i) t[i]+=v;}
+ll ask(aa &t,ll i) {ll R=0; for(; i>0; i-=i&-i) R+=t[i]; return R;}
