712100217bead80f998fe5f3fa48ad300260452b
shihab-2019331068 <69476389+shihab-2019331068@users.noreply.github.com>
2022-05-31 23:06:08 +0600

    Update stree.cpp

--- stree.cpp
+++ stree.cpp
@@ -1,14 +1,21 @@
-ll rnq(ll l,ll r,ll u=1,ll b=1,ll e=n){
-   if(r<b or e<l) return 0;|
-   if(l<=b and e<=r) return T[u];
-   ll m=b+e>>1;
-   return rnq(l,r,u<<1,b,m)+rnq(l,r,u<<1|1,m+1,e);
-}
-
-void upd(ll i,ll v,ll u=1,ll b=1,ll e=n){
-   if(i<b or e<i) return;
-   if(i<=b and e<=i) {T[u]+=v; return;}
-   ll m=b+e>>1;
-   upd(i,v,u<<1,b,m); upd(i,v,u<<1|1,m+1,e);
-   T[u]=T[u<<1]+T[u<<1|1];
-}
+struct SMT {
+   ll tv=0,b=inf,e=-inf,oobVal=-inf;
+   SMT *ls=0,*rs=0;
+   SMT(ll _b,ll _e): b(_b), e(_e) {};
+   SMT(ll _b,ll _e,ll _tv): b(_b), e(_e), tv(_tv) {};
+   ll mrg(ll x,ll y) {
+      return max(x,y);
+   }
+   ll rnq(ll l,ll r) {
+      if(r<b or e<l) return oobVal;
+      if(l<=b and e<=r) return tv;
+      return b<e?mrg(ls->rnq(l,r),rs->rnq(l,r)):tv;
+   }
+   ll upd(ll id,ll val) {
+      if(id<b or e<id) return tv;
+      if(id==b and e==id) { return tv+=val;}
+      if(!ls and b<e) ls=new SMT(b,b+e>>1);
+      if(!rs and b<e) rs=new SMT(b+e+2>>1,e);
+      return tv=mrg(ls->upd(id,val),rs->upd(id,val));
+   }
+};
